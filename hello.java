d036e8bb91335c838c3d67463e2173f14b04c84c
jana <jana273@gmail.com>
2023-12-01 10:43:58 +0530

    some changes

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ class hello{
     public static void main(String[] args) {
         System.out.println("jana
         ");
+         System.out.println("jana2");
         System.out.println("jana
         hii da daii");
     }
